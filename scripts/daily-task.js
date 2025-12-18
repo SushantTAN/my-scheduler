3d98b3090a522844b4ff4392236c714cf08c965b
SushantTAN <punk1susant@gmail.com>
2025-12-18 20:59:31 +0545

    initialized render active script

---
+++ scripts/daily-task.js
@@ -0,0 +1,12 @@
+// scripts/daily-task.js
+console.log('Daily script running at:', new Date().toISOString())
+
+// Example logic
+// - fetch API
+// - clean data
+// - update files
+// - send webhook, etc.
+
+fetch('https://e-commerce-0dit.onrender.com/api/products?search=&categoryId=&sortBy=&sortOrder=ASC&page=1').then(data => { })
+
+process.exit(0)
